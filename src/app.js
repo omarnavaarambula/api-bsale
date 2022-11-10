57639a366d54f011d17f03fdba64b4b715539554
omarnavaarambula <106714513+omarnavaarambula@users.noreply.github.com>
2022-11-10 01:22:36 -0600

    add cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from 'express'
-
+import cors from 'cors'
 import categoryRoutes from './routes/category.routes.js'
 import productRoutes from './routes/product.routes.js'
 
 
 const app = express();
-
+app.use(cors());
 
 
 
