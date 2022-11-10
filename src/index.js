e0dc12e1c828a69416d23fc9ef44175cf4abb80a
omarnavaarambula <106714513+omarnavaarambula@users.noreply.github.com>
2022-11-10 01:21:53 -0600

    add cors

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
-// import express from 'express'
-
-// import categoryRoutes from './routes/category.routes.js'
-// import productRoutes from './routes/product.routes.js'
-// import {PORT} from './config.js'
-
-// const app = express();
-
-
-
-
-// app.use(express.json());
-// app.use('/api/v1/',categoryRoutes);
-// app.use('/api/v1/', productRoutes);
-
-
+import cors from 'cors'
 import app from './app.js'
 import {PORT} from './config.js'
 
-
+app.use(cors());
 app.listen(PORT);
 console.log ('Server running on port ', PORT);
